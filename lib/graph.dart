2112877eb957fac5b5ab602c8913e425ae44c0ea
Krzysztof Dziardziel <krzysztof.dziardziel@students.akademeia.edu.pl>
2020-08-01 04:02:00 +0200

    first commit

---
+++ lib/graph.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:timer_builder/timer_builder.dart';
+
+class GraphPoint {
+  final int x;
+  final int y;
+  GraphPoint(this.x, this.y);
+}
+
+valuesToPoints(List values) {
+  List<GraphPoint> points = new List();
+  for (int i = 0; i < 1023; i++) {
+    points.add(GraphPoint(i, values[i]));
+  }
+  return points;
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    int dataPack = 0;
+    return TimerBuilder.periodic(Duration(seconds: 1), //updates every second
+        builder: (context) {
+      // List<GraphPoint> points0 = valuesToPoints(exampleData[dataPack], 0);
+      // List<GraphPoint> points1 = valuesToPoints(exampleData[dataPack], 1);
+      // List<GraphPoint> points2 = valuesToPoints(exampleData[dataPack], 2);
+      dataPack++;
+      if (dataPack == 32) dataPack = 0;
+      getSeriesData() {
+        List<charts.Series<GraphPoint, int>> series = [
+          charts.Series(
+              id: "0",
+              // data: points0,
+              domainFn: (GraphPoint series, _) => series.x,
+              measureFn: (GraphPoint series, _) => series.y,
+              colorFn: (GraphPoint series, _) =>
+                  charts.MaterialPalette.blue.shadeDefault),
+          charts.Series(
+              id: "1",
+              // data: points1,
+              domainFn: (GraphPoint series, _) => series.x,
+              measureFn: (GraphPoint series, _) => series.y,
+              colorFn: (GraphPoint series, _) =>
+                  charts.MaterialPalette.red.shadeDefault),
+          charts.Series(
+              id: "2",
+              // data: points2,
+              domainFn: (GraphPoint series, _) => series.x,
+              measureFn: (GraphPoint series, _) => series.y,
+              colorFn: (GraphPoint series, _) =>
+                  charts.MaterialPalette.green.shadeDefault)
+        ];
+        return series;
+      }
+
+      return Container(
+        height: height,
+        width: width,
+        child: charts.LineChart(
+          getSeriesData(),
+          animate: false,
+        ),
+      );
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,159 @@
+import 'dart:async';
+// ignore: unused_import
+import 'dart:ffi';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app_esp32_usb_serial/graph.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:usb_serial/transaction.dart';
+import 'package:usb_serial/usb_serial.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  UsbPort _port;
+  // ignore: unused_field
+  String _status = "Idle";
+  List<Widget> _ports = [];
+  List<int> _serialData1 = [];
+  List<GraphPoint> data1 = [];
+  // List<int> _serialData2 = [];
+  // List<int> _serialData3 = [];
+
+  StreamSubscription<String> _subscription;
+  Transaction<String> _transaction;
+  int _deviceId;
+  // ignore: unused_field
+  TextEditingController _textController = TextEditingController();
+
+  Future<bool> _connectTo(device) async {
+    _serialData1.clear();
+
+    if (_subscription != null) {
+      _subscription.cancel();
+      _subscription = null;
+    }
+
+    if (_transaction != null) {
+      _transaction.dispose();
+      _transaction = null;
+    }
+
+    if (_port != null) {
+      _port.close();
+      _port = null;
+    }
+
+    if (device == null) {
+      _deviceId = null;
+      setState(() {
+        _status = "Disconnected";
+      });
+      return true;
+    }
+
+    _port = await device.create();
+    if (!await _port.open()) {
+      setState(() {
+        _status = "Failed to open port";
+      });
+      return false;
+    }
+
+    _deviceId = device.deviceId;
+    await _port.setDTR(true);
+    await _port.setRTS(true);
+    await _port.setPortParameters(
+        100000, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+    _transaction = Transaction.stringTerminated(
+        _port.inputStream, Uint8List.fromList([10]));
+
+    _subscription = _transaction.stream.listen((String line) {
+      setState(() {
+        var workingArr = line.split(' ');
+        _serialData1.add(hexToDec(workingArr[0]));
+        if (_serialData1.length > 1024) _serialData1.clear();
+        // data1.add(valuesToPoints(hexToDec(workingArr[0])));
+        // if (data1.length > 1024) data1.clear();
+      });
+    });
+
+    setState(() {
+      _status = "Connected";
+    });
+    return true;
+  }
+
+  void _getPorts() async {
+    _ports = [];
+    List<UsbDevice> devices = await UsbSerial.listDevices();
+    print(devices);
+
+    devices.forEach((device) {
+      _ports.add(ListTile(
+          leading: Icon(Icons.usb),
+          title: Text(device.productName),
+          subtitle: Text(device.manufacturerName),
+          trailing: RaisedButton(
+            child:
+                Text(_deviceId == device.deviceId ? "Disconnect" : "Connect"),
+            onPressed: () {
+              _connectTo(_deviceId == device.deviceId ? null : device)
+                  .then((res) {
+                _getPorts();
+              });
+            },
+          )));
+    });
+
+    setState(() {
+      print(_ports);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    UsbSerial.usbEventStream.listen((UsbEvent event) {
+      _getPorts();
+    });
+
+    _getPorts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectTo(null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Column(
+          children: <Widget>[
+            ..._ports,
+            Text(_serialData1.toString()),
+            // Graph(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+hexToDec(String hex) {
+  if (int.parse(hex, radix: 16) > 32768) {
+    return int.parse(hex, radix: 16) - 65536;
+  }
+  return int.parse(hex, radix: 16);
+}
